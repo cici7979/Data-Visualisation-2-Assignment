4e361f62c3a31784f2becfc2d8e1c6ae99b9475a
cici7979 <128128320+cici7979@users.noreply.github.com>
2024-10-17 22:34:53 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "https://raw.githubusercontent.com/cici7979/Data-Visualisation-2-Assignment/main/malaysia_population_map.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+  // Access the Vega view instance
+}).catch(console.error);
